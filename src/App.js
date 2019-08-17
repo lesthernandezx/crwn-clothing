afacd8ed79d7c29ed6b64a0e0c20a0c3a036a0e4
lesthernandez <lesthernandez@gmail.com>
2019-08-17 15:09:58 -0600

    created project with create react app and added files for homepage

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import HomePage from './homepage.component';
+
+function App() {
+  return (
+    <HomePage></HomePage>
+  );
+    
+  
+}
+
+export default App;
