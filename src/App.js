6789cb8da8353bc28f3d862a9d31f643482964c4
lesthernandezx <lesthernandez@gmail.com>
2019-08-27 20:35:10 -0600

    adding header component and links

--- src/App.js
+++ src/App.js
@@ -5,35 +5,14 @@ import {Switch} from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-
-);
-
-const TopicsPage = () => (
-  <div>
-    <h1>TOPICS</h1>
-  </div>
-
-);
-
-const TopicsDetailPage = () => (
-  <div>
-    <h1>TOPICS DETAIL</h1>
-  </div>
-
-);
+import HeaderComponent from './components/header/header.component.jsx';
 
 function App() {
   return (
     <div>
+      <HeaderComponent/>
       <Switch>
         <Route exact={true} path='/' component= {HomePage}/>
-        <Route exact={true} path='/hats' component= {HatsPage}/>
         <Route exact={true} path='/shop' component= {ShopPage}/>
       </Switch>
     </div>
